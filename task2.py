f5f2c4e35edd4185d7c4567ae18ad815f07b0a4b
olha.senchyk <olhasenchyk@gmail.com>
2024-01-07 13:06:45 +0100

    edit

--- task2.py
+++ task2.py
@@ -1,44 +1,40 @@
 import turtle
 
-def draw_colored_line(length, color, width):
+def draw_colored_line(length, color, width, level):
     turtle.pencolor(color)
     turtle.pensize(width)
-    if length < 5:
+    if level == 0:
         turtle.forward(length)
     else:
         new_length = length / 3.0
-        draw_colored_line(new_length, color, width)
+        draw_colored_line(new_length, color, width, level - 1)
         turtle.left(60)
-        draw_colored_line(new_length, color, width)
+        draw_colored_line(new_length, color, width, level - 1)
         turtle.right(120)
-        draw_colored_line(new_length, color, width)
+        draw_colored_line(new_length, color, width, level - 1)
         turtle.left(60)
-        draw_colored_line(new_length, color, width)
+        draw_colored_line(new_length, color, width, level - 1)
 
 def koch_snowflake(level, length, color, width):
     for _ in range(3):
-        draw_colored_line(length, color, width)
+        draw_colored_line(length, color, width, level)
         turtle.right(120)
-    turtle.hideturtle()
 
 try:
     level = int(input("Введіть рівень рекурсії (додатне ціле число): "))
     if level < 0:
         raise ValueError()
-    if not isinstance(level, int):
-        raise ValueError()
 except ValueError:
     print("Будь ласка, введіть додатне ціле число.")
     exit()
-    
 
 turtle.bgcolor("#192a32") 
 turtle.speed(0)
 turtle.penup()
-turtle.goto(-200, 100)
+turtle.goto(-150, 90)
 turtle.pendown()
-turtle.shape("turtle") 
 
-koch_snowflake(level, 400, "pink", 2) 
+koch_snowflake(level, 300, "pink", 2)
 
+turtle.hideturtle() 
 turtle.done()
